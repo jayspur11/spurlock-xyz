a3bbdf597f01a122522fbb30305584a0e60a1e93
Jaysen Spurlock <jayspur11@gmail.com>
2021-02-07 22:01:39 -0800

    Remove secret key -- don't need it yet, wasn't doing it right anyway.

--- main/__init__.py
+++ main/__init__.py
@@ -4,9 +4,6 @@ from flask import Flask
 
 def create_app(test_config=None):
     app = Flask(__name__)
-    app.config.from_mapping(
-        SECRET_KEY = os.environ.get("SECRET_KEY", "dev")
-    )
 
     @app.route("/")
     def hello():
